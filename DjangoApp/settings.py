99a0adfe64878e577c1422a1eb993da3b8e834e8
sudaneee <sudaniy65@gmail.com>
2025-01-08 11:54:00 +0100

    static still updates

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Should match the path in Nginx
 
-# Use Django's default static files storage (no Whitenoise)
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
+# Media files (uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
