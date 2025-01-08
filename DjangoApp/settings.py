de3c507d3b7d84f92c7dfff464b3a06841e3801f
sudaneee <sudaniy65@gmail.com>
2025-01-08 12:17:54 +0100

    another settings

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o6_a-(vu9p6hwbd1^!ro)lvfk47hi*d38q+kr1nl0x+dy4_08('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,12 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/giia/static/'
+
+# When you collect static files, they will be stored in this directory
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/root/giia/media/'
+
+# Directory where media files will be stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
