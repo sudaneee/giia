6ad60666e2e99ce8521c70e3325530adbadcde59
sudaneee <sudaniy65@gmail.com>
2025-01-08 12:01:17 +0100

    dawo da static and media

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Should match the path in Nginx
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Media files (uploaded files)
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
